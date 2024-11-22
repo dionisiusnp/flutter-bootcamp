57f29577f5478270bb0be958e18a6238d5b6d257
Abel Indra <abelindraprawito@gmail.com>
2024-11-22 13:29:10 +0700

    update

--- lib/model/order_item_model.dart
+++ lib/model/order_item_model.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 class OrderItemModel {
+  int? id;
   int? order_id;
   String? product_id;
   int? quantity;
@@ -8,19 +9,19 @@ class OrderItemModel {
   int? shipping_cost;
   int? total_sub;
 
-  OrderItemModel({this.order_id, this.product_id, this.quantity, this.price, this.shipping_cost, this.total_sub});
+  OrderItemModel({this.id, this.order_id, this.product_id, this.quantity, this.price, this.shipping_cost, this.total_sub});
   factory OrderItemModel.fromJson(Map<String, dynamic> map) {
     return OrderItemModel(
-        order_id: map["order_id"], product_id: map["product_id"], quantity: map["quantity"], price: map["price"], shipping_cost: map["shipping_cost"], total_sub: map["total_sub"]);
+        id: map["id"], order_id: map["order_id"], product_id: map["product_id"], quantity: map["quantity"], price: map["price"], shipping_cost: map["shipping_cost"], total_sub: map["total_sub"]);
   }
 
   Map<String, dynamic> toJson() {
-    return {"order_id": order_id, "product_id": product_id, "quantity": quantity, "price": price, "shipping_cost": shipping_cost, "total_sub": total_sub};
+    return {"id": id, "order_id": order_id, "product_id": product_id, "quantity": quantity, "price": price, "shipping_cost": shipping_cost, "total_sub": total_sub};
   }
 
   @override
   String toString() {
-    return 'OrderItemModel{order_id: $order_id, product_id: $product_id, quantity: $quantity, price: $price, shipping_cost: $shipping_cost, total_sub: $total_sub}';
+    return 'OrderItemModel{id: $id, order_id: $order_id, product_id: $product_id, quantity: $quantity, price: $price, shipping_cost: $shipping_cost, total_sub: $total_sub}';
   }
 }
 
